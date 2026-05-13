14615e303b88db42a9f3f6d301be188eec94660b
Alexsandra de Souza Arrais <a.alexsandrasouzaarrais@gmail.com>
2026-05-13 16:24:48 -0300

    Add files via upload

---
+++ TestedeMesa3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestedeMesa03
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] v = new int[8];
+            int a = 7;
+            int b = a - 6;
+
+            while (b < a)
+            {
+                v[b] = b + a;
+                Console.WriteLine(v[b]);
+                b = b + 2;
+            }
+        }
+    }
+}
