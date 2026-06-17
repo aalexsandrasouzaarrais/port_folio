e38508e0a3c1e13bae1bec3809c149d55243f83a
Alexsandra Arrais <a.alexsandrasouzaarrais@gmail.com>
2026-06-17 16:54:12 -0300

    Portfolio funcional

---
+++ testedemesa2.2/testedemesa2_2/Program.cs
@@ -0,0 +1,58 @@
+﻿
+/* Dada a fórmula do VALOR FUTURO (juros compostos a.m.) para cálcular investimento:   F = p * ( 1 + i ) ^n 
+p= valor presente
+i= taxa de juros
+n= tempo a.m
+f=valor final do investimento
+*/
+using System;
+namespace TestedeMesa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double p;
+            double i;
+            double n;
+            string tipon;
+            
+            double r;/*rendimento, valor final*/
+            double rl;/*rendimento líquido*/
+            double ra;/*rendimento acumulado*/
+
+            Console.WriteLine("Digite o valor do investimento em reais (R$):");
+            p = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual a taxa de juros? em %");
+            i = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Você quer investir em meses ou em anos?(meses/anos)");
+            tipon = Console.ReadLine()?.ToLower() ?? "";
+
+            Console.WriteLine("Quanto tempo você quer investir?");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            if (tipon == "anos")
+
+            {
+                n = n * 12;
+            }
+
+            
+
+            r = p * Math.Pow(1 + (i / 100), n);
+            rl = r - p;
+            ra = r ; 
+
+
+
+
+            
+
+        }
+    }
+}
+
+
